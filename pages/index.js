8b23a43c3ddf2f47350128c991ba7999b7893a8c
Harry Kaung <mirrorsyncofficial@gmail.com>
2025-10-06 20:02:27 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <div>
+    <div style={{ padding: '50px', textAlign: 'center' }}>
       <h1>Welcome to MirrorSync</h1>
-      <p>Your home page content here</p>
+      <p>Sync your files seamlessly across all your devices</p>
+      <button>Get Started</button>
     </div>
   );
 }
