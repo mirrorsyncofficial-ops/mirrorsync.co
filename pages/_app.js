07b2ad9b53ea3351f022c98f825d77fa3a7e3060
Harry Kaung <mirrorsyncofficial.com>
2025-10-07 17:32:26 +0700

    Fix merge conflict in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-import '../styles/globals.css'
-
-   function MyApp({ Component, pageProps }) {
-     return <Component {...pageProps} />
-   }
-
-   export default MyApp
-=======
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
->>>>>>> 8bc6359 (Add beautiful landing page with inline styles)
+ 8bc6359 (Add beautiful landing page with inline styles)
